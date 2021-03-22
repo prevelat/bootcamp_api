27df33c82c29d4383e1b250d0463534c35924539
Felipe Prevelato <prevelat3@gmail.com>
2021-03-21 23:52:38 -0300

    added custom ErrorResponse class and mongoose error handling

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,4 @@
+const ErrorResponse = require('../Utils/errorResponse');
 const Bootcamp = require('../models/Bootcamps');
 
 // @desc        Get all bootcamps
@@ -8,8 +9,8 @@ exports.getBootcamps = async (req, res, next) => {
         const bootcamps = await Bootcamp.find();
 
         res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
-    } catch (error) {
-        res.status(400).json({ success: false });
+    } catch (err) {
+        next(err);
     }
 };
 
@@ -21,13 +22,12 @@ exports.getBootcamp = async (req, res, next) => {
         const bootcamp = await Bootcamp.findById(req.params.id);
 
         if (!bootcamp) {
-            return res.status(400).json({ success: false });
+            return next(new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404));
         }
 
         res.status(200).json({ success: true, data: bootcamp });
-    } catch (error) {
-        // res.status(400).json({ success: false });
-        next(error);
+    } catch (err) {
+        next(err);
     }
 };
 
@@ -39,8 +39,8 @@ exports.createBootcamp = async (req, res, next) => {
         const bootcamp = await Bootcamp.create(req.body);
 
         res.status(201).json({ success: true, data: bootcamp });
-    } catch (error) {;
-        res.status(400).json({ success: false });
+    } catch (err) {;
+        next(err);
     }
 };
 
@@ -52,12 +52,12 @@ exports.updateBootcamp = async (req, res, next) => {
         const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
 
         if (!bootcamp) {
-            return res.status(400).json({ success: false });
+            return next(new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404));
         }
 
         res.status(200).json({ success: true, data: bootcamp });
-    } catch (error) {
-        res.status(400).json({ success: false });
+    } catch (err) {
+        next(err);
     }
 };
 
@@ -69,11 +69,11 @@ exports.deleteBootcamp = async (req, res, next) => {
         const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
         if (!bootcamp) {
-            return res.status(400).json({ success: false });
+            return next(new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404));
         }
 
         res.status(200).json({ success: true, data: {} });
-    } catch (error) {
-        res.status(400).json({ success: false });
+    } catch (err) {
+        next(err);
     }
 };
