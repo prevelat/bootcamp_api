d2fa34df6780061c946af8cbd66fd089ad2f4d6e
Felipe Prevelato <prevelat3@gmail.com>
2021-03-24 04:57:21 -0300

    added bootcamp-user relationship

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -24,10 +24,21 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
         res.status(200).json({ success: true, data: bootcamp });
 });
 
-// @desc        Create bootcamp
+// @desc        Create new bootcamp
 // @route       POST /api/v1/bootcamps/:id
 // @access      Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+        // Add user to req.body
+        req.body.user = req.user.id;
+
+        // Check for published bootcamp
+        const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+
+        // If the user is not admin, they can only create one bootcamp
+        if (publishedBootcamp && req.user.role !== 'admin') {
+                return next(new ErrorResponse(`The user with ID ${req.user.id} has already published a bootcamp`, 400));
+        }
+
         const bootcamp = await Bootcamp.create(req.body);
 
         res.status(201).json({ success: true, data: bootcamp });
--- models/User.js
+++ models/User.js
@@ -23,7 +23,7 @@ const UserSchema = new mongoose.Schema({
       password: {
         type: String,
         required: [true, 'Please add a password'],
-        minlength: 8,
+        minlength: 6,
         select: false
       },
       resetPasswordToken: String,
