618801097b96bfe78eabfa16c6646d603513ee05
Felipe Prevelato <prevelat3@gmail.com>
2021-03-24 00:19:19 -0300

    added espress-fileupload & photo upload router + controller

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,4 @@
+path = require('path');
 const ErrorResponse = require('../Utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const geocoder = require('../utils/geocoder');
@@ -147,5 +148,49 @@ exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
                 success: true,
                 count: bootcamps.length,
                 data: bootcamps
+        });
+});
+
+// @desc        Upload bootcamp photo
+// @route       PUT /api/v1/bootcamps/:id/photo
+// @access      Private
+exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+
+        if (!bootcamp) {
+            return next(new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404));
+        }
+
+        if (!req.files) {
+                return next(new ErrorResponse(`Please upload a file`, 400));
+        }
+
+        const file = req.files.file;
+
+        // Make sure that the file is an image
+        if (!file.mimetype.startsWith('image')) {
+                return next(new ErrorResponse(`Please upload an image file`, 400));
+        }
+
+        // Check if file is under max size
+        if (file.size > process.env.MAX_FILE_UPLOAD_SIZE) {
+                return next(new ErrorResponse(`Please upload an image with less than ${process.env.MAX_FILE_UPLOAD_SIZE}`, 400));
+        }
+
+        // Create custom file name
+        file.name = `photo_${bootcamp._id}${path.parse(file.name).ext}`;
+
+        file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async err => {
+                if (err) {
+                        console.error(err);
+                        return next(new ErrorResponse(`Problem with file upload`, 400));
+                }
+
+                await Bootcamp.findByIdAndUpdate(req.params.id, { photo: file.name });
+
+                res.status(200).json({
+                        succes: true,
+                        data: file.name
+                });
         });
 });
