2b60fa19c4a2491837714347c2a7cb3b0a3497a4
Felipe Prevelato <prevelat3@gmail.com>
2021-03-24 19:45:18 -0300

    added nodemailer and forgot password / POST (generate token + send email)

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
@@ -36,6 +37,10 @@ const UserSchema = new mongoose.Schema({
 
 // Encrypt pass with bvryptjs
 UserSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
   const salt = await bcryptjs.genSalt(10);
   this.password = await bcryptjs.hash(this.password, salt);
 });
@@ -48,6 +53,20 @@ UserSchema.methods.getSignedJwtToken = function() {
 // Match user entered password to hashed password in DB
 UserSchema.methods.matchPassword = async function(enteredPassword) {
   return await bcryptjs.compare(enteredPassword, this.password);
+};
+
+// Generate and hash passwrod token
+UserSchema.methods.getResetPasswordToken = function() {
+  //Generate token
+  const resetToken = crypto.randomBytes(20).toString('hex');
+
+  //Hash token and set to resetPasswordToken field
+  this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+
+  // Set expire
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
 };
 
 module.exports = mongoose.model('User', UserSchema);
