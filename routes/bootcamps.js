91d97219fbcededa86cda9e86243a84df59d28f0
Felipe Prevelato <prevelat3@gmail.com>
2021-03-24 04:30:27 -0300

    added role authorization

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -18,7 +18,7 @@ const courseRouter = require('./courses');
 
 const router = express.Router();
 
-const { protect } = require('../middleware/auth');
+const { protect, authorize } = require('../middleware/auth');
 
 // Re-route into other resources routers
 router.use('/:bootcampId/courses', courseRouter);
@@ -27,15 +27,15 @@ router.route('/radius/:zipcode/:distance')
     .get(getBootcampsInRadius);
 
 router.route('/:id/photo')
-    .put(protect, bootcampPhotoUpload);
+    .put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload);
 
 router.route('/')
     .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
-    .post(protect, createBootcamp);
+    .post(protect, authorize('publisher', 'admin'), createBootcamp);
 
 router.route('/:id')
     .get(getBootcamp)
-    .put(protect, updateBootcamp)
-    .delete(protect, deleteBootcamp);
+    .put(protect, authorize('publisher', 'admin'), updateBootcamp)
+    .delete(protect, authorize('publisher', 'admin'), deleteBootcamp);
 
 module.exports = router;
