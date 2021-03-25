917c56d7aa6c8b54dc94f6695c4c602d3e43abeb
Felipe Prevelato <prevelat3@gmail.com>
2021-03-25 04:00:33 -0300

    added logout request to clear cookie token if used

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,9 +7,11 @@ const User = require('../models/User');
 exports.protect = asyncHandler(async (req, res, next) => {
     let token;
 
+    // Get token from Beare header
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         token = req.headers.authorization.split(' ')[1];
     }
+    // Get token from cookie
     // else if (req.cookies.token) {
     //     token = req.cookies.token;
     // }
