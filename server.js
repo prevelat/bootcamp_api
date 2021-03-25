51308a619eb75db3ec1f8aed918bf58f9488dd89
Felipe Prevelato <prevelat3@gmail.com>
2021-03-25 04:18:58 -0300

    added XSS protection (xss-clean) and security headers (helmet)

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
-const mongoSanitize = require('express-mongo-sanitize');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean')
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -40,6 +42,12 @@ app.use(fileupload());
 
 // Sanitize data
 app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
